d4c3735921f68b60a5c23df10b2cb1bd8f37da13
Matt <44251435+matt-degraffenreid@users.noreply.github.com>
2024-10-18 17:30:47 -0600

    Add agent assist service configuration

--- flex-template-agent-assist/src/feature-library/agent-assist/types/ServiceConfiguration.ts
+++ flex-template-agent-assist/src/feature-library/agent-assist/types/ServiceConfiguration.ts
@@ -1,3 +1,14 @@
 export default interface AgentAssistConfig {
   enabled: boolean;
+  custom_api_endpoint: string;
+  conversation_profile: string;
+  conversation_summary: boolean;
+  agent_coaching: boolean;
+  proactive_general_knowleadge_assist: boolean;
+  smart_reply: boolean;
+  enable_voice: boolean;
+  notifier_server_endpoint: string;
+  transcription: boolean;
+  intermediate_transcription: boolean;
+  debug: boolean;
 }
