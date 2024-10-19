ea2bbb97b0549f028ba7ac268809c027cf1dc657
Matt <44251435+matt-degraffenreid@users.noreply.github.com>
2024-10-18 19:14:33 -0600

    Add additional configuration for agent assist

--- flex-template-agent-assist/src/feature-library/agent-assist/config.ts
+++ flex-template-agent-assist/src/feature-library/agent-assist/config.ts
@@ -1,19 +1,19 @@
 import { getFeatureFlags } from '../../utils/configuration';
 import AgentAssistConfig from './types/ServiceConfiguration';
 
-const { 
+const {
   enabled = false,
   custom_api_endpoint = '',
   conversation_profile = '',
-  conversation_summary =  true,
-  agent_coaching =  true,
-  proactive_general_knowleadge_assist =  true,
-  smart_reply =  true,
-  enable_voice =  false,
+  conversation_summary = true,
+  agent_coaching = true,
+  proactive_general_knowleadge_assist = true,
+  smart_reply = true,
+  enable_voice = false,
   notifier_server_endpoint = '',
-  transcription =  false,
-  intermediate_transcription =  false,
-  debug =  false,
+  transcription = false,
+  intermediate_transcription = false,
+  debug = false,
 } = (getFeatureFlags()?.features?.agent_assist as AgentAssistConfig) || {};
 
 export const isFeatureEnabled = () => {
@@ -22,44 +22,44 @@ export const isFeatureEnabled = () => {
 
 export const getCustomApiEndpoint = () => {
   return custom_api_endpoint;
-}
+};
 
 export const getConversationProfile = () => {
   return conversation_profile;
-}
+};
 
 export const isConversationSummaryEnabled = () => {
   return conversation_summary;
-}
+};
 
 export const isAgentCoachingEnabled = () => {
   return agent_coaching;
-}
+};
 
 export const isPGKAEnabled = () => {
   return proactive_general_knowleadge_assist;
-}
+};
 
 export const isSmartReplyEnabled = () => {
   return smart_reply;
-}
+};
 
 export const isVoiceEnabled = () => {
   return enable_voice;
-}
+};
 
 export const getNotifierServerEndpoint = () => {
   return notifier_server_endpoint;
-}
+};
 
 export const isTranscriptionEnabled = () => {
   return transcription;
-}
+};
 
 export const isIntermediateTranscriptionEnabled = () => {
   return intermediate_transcription;
-}
+};
 
 export const isDebugEnabled = () => {
   return debug;
-}
+};
